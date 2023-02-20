ed8a2f41c0bd85fbc667c6529787371f4ef17b6e
programandoconcabeza <programandoconcabeza@gmail.com>
2023-02-20 16:49:20 +0100

    quit comments and include more information in composer.json

--- src/PHPColorCli.php
+++ src/PHPColorCli.php
@@ -8,43 +8,35 @@ final class PHPColorCli
 {
     use ColorsTrait;
 
-    // Returns colored string
     public static function getColoredString($string, $text_color = null, $background_color = null, $option = null): string
     {
         $colored_string = "";
 
-        // Check if given foreground color found
         if ($text_color && self::returnColor($text_color) !== null) {
             $colored_string .= "\033[" . self::returnColor($text_color) . "m";
         }
-        // Check if given background color found
         if ($background_color && self::returnBackground($background_color) !== null) {
             $colored_string .= "\033[" . self::returnBackground($background_color) . "m";
         }
 
-        // Check if given option found
         if ($option && self::returnOption($option) !== null) {
             $colored_string .= "\033[" . self::returnOption($option) . "m";
         }
 
-        // Add string and end coloring
         $colored_string .=  $string . "\033[0m";
         return $colored_string;
     }
 
-    // Returns all foreground color names
     public static function getForegroundColors(): array
     {
         return array_keys(self::arrayColors());
     }
 
-    // Returns all background color names
     public static function getBackgroundColors(): array
     {
         return array_keys(self::arrayBackground());
     }
 
-    // Returns all background color names
     public static function getOptions(): array
     {
         return array_keys(self::arrayOptions());
